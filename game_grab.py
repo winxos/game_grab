bca0623d44226e35db9ca17e179f8442ccf1ccdd
winxos <winxos@hotmail.com>
2017-03-02 00:05:46 +0800

    添加下载进度，修复detail bug

--- game_grab.py
+++ game_grab.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 '''
 game factory scraper
-env:python 3.5
+env:python 3.6 + lxml
 license:MIT
 author:winxos
 since:2017-02-19
@@ -16,24 +16,26 @@ import traceback
 from functools import partial
 
 POOLS_SIZE = 20
-try_times = 3
+TRY_TIMES = 3
+SINGLE_THREAD_DEBUG = False
+# SINGLE_THREAD_DEBUG = True
 
 with open('grab_config.json', 'r', encoding='utf-8') as f:
     CONFIG = json.load(f)
 
 
 def get_content(url, xpath, charset):
-    global try_times
+    global TRY_TIMES
     try:
         f = urllib.request.urlopen(url)
         c = etree.HTML(f.read().decode(charset))
-        try_times = 3
+        TRY_TIMES = 3
         return c.xpath(xpath)
     except Exception as e:  # 捕捉访问异常，一般为timeout，信息在e中
         print("[err] %s" % url)
         print(traceback.format_exc())
-        try_times -= 1
-        if try_times > 0:
+        TRY_TIMES -= 1
+        if TRY_TIMES > 0:
             return get_content(url, xpath, charset)
         return None
 
@@ -55,8 +57,7 @@ def get_game_info(RULE_ID, url):
     game_name = "".join(get_content(url, CONFIG["rules"][RULE_ID]["game_name"],
                                     CONFIG["rules"][RULE_ID]["charset"]))
     game_detail = "".join([x.strip() for x in
-                           get_content(url,
-                                       CONFIG["rules"][RULE_ID]["game_detail"],
+                           get_content(url, CONFIG["rules"][RULE_ID]["game_detail"],
                                        CONFIG["rules"][RULE_ID]["charset"])])
     game_src = "".join(get_content(url, CONFIG["rules"][RULE_ID]["game_src"],
                                    CONFIG["rules"][RULE_ID]["charset"]))
@@ -71,21 +72,36 @@ def save_txt(name, data, mode='a'):
 def download(rule_id):
     st = clock()
     pool = Pool(processes=POOLS_SIZE)
-    print("[debug] downloading game list.")
+    print("[debug] downloading game pages list.")
+    page_urls = get_pages(rule_id)
     func = partial(get_page_games, rule_id)
-    game_list = pool.map(func, get_pages(rule_id))
-    # pool.close()
-    # pool.join()
+    game_list = []
+    pi = pool.imap(func, page_urls)  # iterator for progress
+    for i in range(len(page_urls)):
+        game_list.append(pi.next())
+        if i % 10 == 0:
+            print("[debug] downloaded pages [%d/%d]" % (i, len(page_urls)))
     game_list = list(itertools.chain(*game_list))
     print('[debug] downloaded %d game urls. used:%f s' %
           (len(game_list), clock() - st))
     print('[debug] downloading game details, waiting.')
     func = partial(get_game_info, 0)
-    games_info = pool.map(func, game_list)
-    pool.close()
-    pool.join()
+    if not SINGLE_THREAD_DEBUG:
+        games_info = []
+        gi = pool.imap(func, game_list)
+        for i in range(len(game_list)):
+            games_info.append(gi.next())
+            if i % 100 == 0:
+                print("[debug] downloading progress %.2f%%" %
+                      (i * 100 / len(game_list)))
+    else:
+        games_info = []
+        for gl in game_list[:10]:
+            games_info.append(func(gl))
+    print('[debug] downloaded %d game details. total used:%f s' %
+          (len(games_info), clock() - st))
     for gi in games_info:
-        save_txt(str(rule_id) + ".txt", str(gi) + "\n")
+        save_txt(str(rule_id) + ".txt", '$'.join(map(str, gi)) + "\n")
 
 
 '''
